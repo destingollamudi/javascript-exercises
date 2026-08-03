297c99546d0ed9c03b4128c5603432da1f9473e5
destingollamudi <destingollamudi@gmail.com>
2026-08-03 17:21:44 -0400

    Implement sumAll function with input validation and enable related tests

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+  if (typeof(a) != Number || typeof(b) != Number){
+    return 'ERROR';
+  }
+  if (a < 0 || b < 0){
+    return 'ERROR';
+  }
 };
 
 // Do not edit below this line
